a7f90384f1dac6c860b52dca1ac102e11aaaff88
Adam Sunshine <adam@csuac.com>
2015-11-20 18:04:38 -0800

    added User edit, update, delete, destroy functionality

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ gem 'rails',                '4.2.2'
 gem 'bcrypt',               '3.1.7'
 gem 'bootstrap-sass',       '3.2.0.0'
 
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 gem 'rails',        '4.2.2'
 gem 'sass-rails',   '5.0.2'
 gem 'uglifier',     '2.5.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Adam",
+             email: "boss@boss.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
